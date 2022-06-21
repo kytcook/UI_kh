e42915a4b1fc21799826e9ef097b6e618d48c854
DESKTOP-G09GV65\user1 <kytcook@naver.com>
2022-06-21 09:41:22 +0900

    20220621

---
+++ day2/hello.js
@@ -0,0 +1,5 @@
+let colors = require("colors"); //
+// html에서는 사용하기 힘든 기능을 오픈소스로 사용할 수 있다. 
+console.log(colors.rainbow("Hello nodeJS!!!")); //clg(자동입력)
+console.log(colors.red("Hello nodeJS!!!")); //clg(자동입력)
+console.log(colors.green("Hello nodeJS!!!")); //clg(자동입력)
