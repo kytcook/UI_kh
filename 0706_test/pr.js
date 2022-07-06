323215d005e0323634dabea913f7ca97a3478a7c
DESKTOP-C5S803A\kytcook <kytcook@naver.com>
2022-07-06 21:17:17 +0900

    study_study_220706

---
+++ 0706_test/pr.js
@@ -0,0 +1,15 @@
+// innerHTML
+// querySelector
+// `${}`
+// 방법1 innerHTML 사용법
+// let body = document.querySelector('body'); /*  */
+// body.innerHTML = '<p>야호야호</p>';
+
+// 방법2 createElement
+let h1 = document.createElement('h1');
+h1.innerHTML = "임은택"
+document.querySelector('body').appendChild(h1);
+
+
+
+
