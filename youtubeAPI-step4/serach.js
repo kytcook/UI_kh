8aed3e5b775c52b009412c4281d7a96df0bb37b8
DESKTOP-C5S803A\kytcook <kytcook@naver.com>
2022-07-07 15:52:24 +0900

    0707

--- youtubeAPI-step4/serach.js
+++ youtubeAPI-step4/serach.js
@@ -14,19 +14,30 @@ var requestOptions = {
 };
 
 fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=AIzaSyBTwWoYZPb-scjc5J4nTEnuee6FOH52bWI&type=video`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
+  .then(response => response.jason())
+  // .then(result => console.log(result))
+  .then(result => {
+    const items = result.items;
+    const videoList = [];
+    videoList.push(`<ul class='videos'>`);
+    for(let i=0; i<items.length; i++){
+      videoList.push(`<li class = 'container'>`);/* ul태그안에 li가 담긴다 */
+      videoList.push(`<div class = 'video'>`);/* ul태그안에 li가 담긴다 */
+      videoList.push(`<img class ='thumbnail' src='${items[i].snippet.thumbnails.medium.url}' alt='이미지'>`);
+      videoList.push(`<div>`); /* 반복문 안쪽에 넣어서 순서가 주루룩 */
+      videoList.push(`<p class='title'>${items[i].snippet.title}</p>`);
+      videoList.push(`<p class='channel'>${items[i].snippet.channelTitle}</p>`);
+      videoList.push(`</div>`);
+      videoList.push(`</div>`); 
+    }
+    videoList.push(`</ul>`);
+    document.querySelector('#root').innerHTML = videoList.join("");
+  })
   .catch(error => console.log('error', error));
 
 /*
-const videoList = [];
 const items = result.items;
 console.log(items.length);
-videoList.push(`<ul class='videos'>`);
-for(let i=0; i<items.length; i++){
-}
-videoList.push(`</ul>`);
-document.querySelector('#root').innerHTML = videoList.join("");
 */
   document.querySelector('#root').innerHTML = '111';
 } ////////end of searchList
